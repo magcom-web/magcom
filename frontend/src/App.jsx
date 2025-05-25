7cf5f59275f313daecf73cd26424e77d3ca929a8
KEERTHANA-321 <keerthanasmanikkoth@gmail.com>
2025-05-25 22:07:25 +0530

    added background gradient

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen  bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/Gradient.webp')" }}>
       <NavBar />
       <main className="flex-grow">
         <AppRoutes />
