331054ec02dd3104067db7bfc2eb58d5f8743694
Athira-Antony <athirathereseantony@gamil.com>
2025-05-27 13:59:09 +0530

    Added SGB

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Articles from './pages/Articles';
-
-
+import Governance from './pages/Sgb';
 
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/articles" element={<Articles />} />
+    <Route path="/sgb-records" element={<Governance/>}></Route>
   </Routes>
 );
 
