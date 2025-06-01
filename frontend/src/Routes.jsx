cbcd6af7e8ed1ef400cb8288dfc37332cb13ef02
Durga-Pillai <durgagmd18@gmail.com>
2025-06-01 11:25:16 +0530

    Add archive page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Articles from './pages/Articles/Articles';
 import Governance from './pages/Sgb/Sgb';
 import ArticlePage from './pages/Articles/ArticlePage';
-
+import ArchiveSlider from './pages/Archives/ArchiveSlider';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/articles" element={<Articles />} />
     <Route path="/sgb-records" element={<Governance/>}></Route>
     <Route path="/articles/:id" element={<ArticlePage />} />
+    <Route path="/previous-editions" element={<ArchiveSlider />} />
   </Routes>
 );
 
