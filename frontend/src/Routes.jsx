08e181ddb8a0f6fed69c5e148c1f5f8b0332bff0
Nitha Shifana <nithashifana@gmail.com>
2025-06-02 17:43:21 +0530

    team page added

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -4,6 +4,7 @@ import Articles from './pages/Articles/Articles';
 import Governance from './pages/Sgb/Sgb';
 import ArticlePage from './pages/Articles/ArticlePage';
 import ArchiveSlider from './pages/Archives/ArchiveSlider';
+import Team from './components/Team/Team';
 
 const AppRoutes = () => (
   <Routes>
@@ -11,6 +12,7 @@ const AppRoutes = () => (
     <Route path="/sgb-records" element={<Governance/>}></Route>
     <Route path="/articles/:id" element={<ArticlePage />} />
     <Route path="/previous-editions" element={<ArchiveSlider />} />
+    <Route path="/team" element= {<Team />} />
   </Routes>
 );
 
